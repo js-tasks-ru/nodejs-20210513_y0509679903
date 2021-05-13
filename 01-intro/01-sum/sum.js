f42a1439e376cdded5831462e82c3d824a5c6c4d
Iana Kain <iana.kain@Iana.Kain>
2021-05-13 22:19:15 +0300

    sum function implementation

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (!(isNaN(a) && isNaN(b)) && (typeof a === 'number' && typeof b === 'number')) {
+    return a + b;
+  }
+
+  throw new TypeError('Arguments must be numbers.');
 }
 
 module.exports = sum;
