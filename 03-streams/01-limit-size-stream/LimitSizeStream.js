faf70e11b7394a4d958a39d7d0e218bdfe52d4db
Iana Kain <iana.kain@Iana.Kain>
2021-05-23 15:31:38 +0300

    limited size stream solution

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,21 @@
+const fs = require('fs');
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit || 0;
+    this.usedLimit = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    if (chunk.length + this.usedLimit > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      this.usedLimit = this.usedLimit + chunk.length;
+      callback(null, chunk);
+    }
   }
 }
 
