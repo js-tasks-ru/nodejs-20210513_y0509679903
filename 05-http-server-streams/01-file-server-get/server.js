d977c76d6b1b77ce0e2eaf5cb5a8327cd20867ee
Iana Kain <iana.kain@Iana.Kain>
2021-05-26 00:17:28 +0300

    file server - file dispatch solution

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,22 +1,39 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Nested folders are not supported');
+  }
 
-  switch (req.method) {
-    case 'GET':
+  try {
+    const filepath = path.join(__dirname, 'files', pathname);
+    const data = fs.readFileSync(filepath);
 
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+    switch (req.method) {
+      case 'GET':
+        res.statusCode = 200;
+        res.end(data);
+        break;
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end('File not found');
+    } else {
+      res.statusCode = 500;
+      res.end('Server error');
+    }
   }
 });
 
