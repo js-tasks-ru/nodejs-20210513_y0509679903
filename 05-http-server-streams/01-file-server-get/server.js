2f04501deda0f44929c4a471cc87735aaa4b6ec3
Iana Kain <iana.kain@Iana.Kain>
2021-05-30 16:27:49 +0300

    http server get and post solution

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -4,13 +4,17 @@ const path = require('path');
 const fs = require('fs');
 
 const server = new http.Server();
+const withStatus = (res) => (statusCode) => {
+  res.statusCode = statusCode;
+  return res;
+};
 
 server.on('request', (req, res) => {
+  const resStatus = withStatus(res);
   const pathname = url.parse(req.url).pathname.slice(1);
 
   if (pathname.includes('/')) {
-    res.statusCode = 400;
-    res.end('Nested folders are not supported');
+    return resStatus(400).end('Nested folders are not supported');
   }
 
   try {
@@ -19,20 +23,15 @@ server.on('request', (req, res) => {
 
     switch (req.method) {
       case 'GET':
-        res.statusCode = 200;
-        res.end(data);
-        break;
+        return resStatus(200).end(data);
       default:
-        res.statusCode = 501;
-        res.end('Not implemented');
+        return resStatus(501).end('Not implemented');
     }
   } catch (error) {
     if (error.code === 'ENOENT') {
-      res.statusCode = 404;
-      res.end('File not found');
+      return resStatus(404).end('File not found');
     } else {
-      res.statusCode = 500;
-      res.end('Server error');
+      return resStatus(500).end('Server error');
     }
   }
 });
--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,22 +1,66 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
+const noop = () => {};
+const withStatus = (res) => (statusCode) => {
+  res.statusCode = statusCode;
+  return res;
+};
+
 server.on('request', (req, res) => {
+  const resStatus = withStatus(res);
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const transform = new LimitSizeStream({limit: 1000000});
 
-  switch (req.method) {
-    case 'POST':
+  if (pathname.includes('/')) {
+    return resStatus(400).end('Nested folders are not supported');
+  }
 
-      break;
+  if (fs.existsSync(filepath)) {
+    return resStatus(409).end('File already exists');
+  }
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  try {
+    switch (req.method) {
+      case 'POST':
+        const writeStream = fs.createWriteStream(filepath);
+        req
+            .pipe(transform)
+            .pipe(writeStream);
+
+        transform.on('error', function(err) {
+          if (err.code === 'LIMIT_EXCEEDED') {
+            fs.unlink(filepath, noop);
+            writeStream.close();
+            return resStatus(413).end('File limit has been exceeded.');
+          } else {
+            if (err) {
+              return resStatus(500).end('Some error occurred.');
+            }
+          }
+        });
+        req.on('close', () => {
+          if (!writeStream.writableFinished) {
+            fs.unlink(filepath, noop);
+            writeStream.close();
+          }
+        });
+        writeStream.on('finish', () => {
+          return resStatus(201).end('Success');
+        });
+        break;
+
+      default:
+        return resStatus(501).end('Not implemented');
+    }
+  } catch (error) {
+    console.log('error', Object.entries(error));
   }
 });
 
